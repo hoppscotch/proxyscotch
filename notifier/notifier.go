82d5a1af43650bf7e557c0399755b902b29cf722
Thomas Jost <schnouki@schnouki.net>
2024-02-17 00:28:11 +0100

    Use XDG data directories for icons too
    So that the icon can be globally installed too.

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -3,13 +3,21 @@ package notifier
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/adrg/xdg"
 )
 
 func GetIcon() string {
-	return GetIconPath() + "/icon.png"
-}
-
-func GetIconPath() string {
+	// Backward compatibility: use the legacy icons dir if it contains
+	// icon.png
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	return dir + "/icons"
+	legacyFile := filepath.Join(dir, "icons", "icon.png")
+	if _, err := os.Stat(legacyFile); err == nil {
+		return legacyFile
+	}
+
+	// Otherwise, use the file from a standard location, or return an empty
+	// string if not found.
+	path, _ := xdg.SearchDataFile("proxyscotch/icon.png")
+	return path
 }
