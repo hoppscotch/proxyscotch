32f720b22ade51dfecabcd5ea50d2cc2f4e4b20c
Andrew Bastin <andrewbastin.k@gmail.com>
2025-03-07 22:38:06 +0530

    feat: allow proxyscotch container to accept environment variables to override values

--- Dockerfile
+++ Dockerfile
@@ -13,9 +13,10 @@ LABEL maintainer="support@hoppscotch.io"
 
 EXPOSE 9159/tcp
 
+COPY --from=builder --chmod=755 /etc/proxyscotch/container_run.sh /usr/bin/run_proxyscotch
 COPY --from=builder /etc/proxyscotch/out/linux-server/proxyscotch-server-linux-* /usr/bin/proxyscotch
 
 # this should be a standard user with the users group on alpine
-USER 1000:100
+# USER 1000:100
 
-CMD ["proxyscotch", "--host", "0.0.0.0:9159"]
+CMD ["run_proxyscotch"]
---
+++ container_run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Default values
+DEFAULT_HOST="0.0.0.0:9159"
+DEFAULT_TOKEN=""
+DEFAULT_ALLOWED_ORIGINS="*"
+DEFAULT_BANNED_OUTPUTS=""
+DEFAULT_BANNED_DESTS=""
+
+# Process host
+if [ -n "${PROXYSCOTCH_HOST}" ]; then
+  HOST_ARG="--host=${PROXYSCOTCH_HOST}"
+else
+  HOST_ARG="--host=${DEFAULT_HOST}"
+fi
+
+# Process token (only add if env var is set or default is not blank)
+TOKEN_ARG=""
+if [ -n "${PROXYSCOTCH_TOKEN}" ]; then
+  TOKEN_ARG="--token=${PROXYSCOTCH_TOKEN}"
+elif [ -n "${DEFAULT_TOKEN}" ]; then
+  TOKEN_ARG="--token=${DEFAULT_TOKEN}"
+fi
+
+# Process allowed-origins
+if [ -n "${PROXYSCOTCH_ALLOWED_ORIGINS}" ]; then
+  ORIGINS_ARG="--allowed-origins=${PROXYSCOTCH_ALLOWED_ORIGINS}"
+else
+  ORIGINS_ARG="--allowed-origins=${DEFAULT_ALLOWED_ORIGINS}"
+fi
+
+# Process banned-outputs (only add if env var is set or default is not blank)
+BANNED_OUTPUTS_ARG=""
+if [ -n "${PROXYSCOTCH_BANNED_OUTPUTS}" ]; then
+  BANNED_OUTPUTS_ARG="--banned-outputs=${PROXYSCOTCH_BANNED_OUTPUTS}"
+elif [ -n "${DEFAULT_BANNED_OUTPUTS}" ]; then
+  BANNED_OUTPUTS_ARG="--banned-outputs=${DEFAULT_BANNED_OUTPUTS}"
+fi
+
+# Process banned-dests (only add if env var is set or default is not blank)
+BANNED_DESTS_ARG=""
+if [ -n "${PROXYSCOTCH_BANNED_DESTS}" ]; then
+  BANNED_DESTS_ARG="--banned-dests=${PROXYSCOTCH_BANNED_DESTS}"
+elif [ -n "${DEFAULT_BANNED_DESTS}" ]; then
+  BANNED_DESTS_ARG="--banned-dests=${DEFAULT_BANNED_DESTS}"
+fi
+
+# Execute the command with the arguments
+./proxyscotch $HOST_ARG $TOKEN_ARG $ORIGINS_ARG $BANNED_OUTPUTS_ARG $BANNED_DESTS_ARG
